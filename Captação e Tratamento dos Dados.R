a81ad9e845bc722d0880e254dca7b4fc5206117a
Joao Videira <joaoamvideira@gmail.com>
2025-01-13 18:28:20 -0300

    Foram adicionados mais pacotes
    Carregou-se a base de dados dos municípios do Rj, com o pacote geobr

--- Captação e Tratamento dos Dados.R
+++ Captação e Tratamento dos Dados.R
@@ -1,2 +1,22 @@
 # ---- Configurações
 library(tidyverse)
+library(raster)
+library(terra)
+library(sp)
+library(sf)
+library(leaflet)
+library(spdep)
+library(geobr)
+library(foreign)
+library(readxl)
+library(stringdist)
+library(ggplot2)
+library(ggspatial)
+library(units)
+library(RColorBrewer)
+
+#---- Obtendo bases de dados
+# Pacote geobr: dados sobre municipíos e UCs do estado
+
+
+# MAPBiomas: série histórica parques estaduais RJ (1985-2023)
