45a4f6dda26a11f030b1a1a71331cfa62aeef5fa
Joao Videira <joaoamvideira@gmail.com>
2025-01-14 18:05:52 -0300

    Foi finalizada a etapa de construção da base com as informações sobre cobertura e uso do solo dos parques estaduais do Rio de Janeiro
    Vale ressaltar que em ambas as bases utilizadas, houve a ausencia dos parques estaduais da Pedra Branca e dos três picos
    
    Foi criado um mapa apresentando os parques distribuis territorialmente pelo estado do Rio de Janeiro

--- Captação e Tratamento dos Dados.R
+++ Captação e Tratamento dos Dados.R
@@ -1,4 +1,4 @@
-# ---- Configurações
+# ---- Configurações-----------------------------------------------------------------------
 library(tidyverse)
 library(raster)
 library(terra)
@@ -15,9 +15,12 @@ library(ggspatial)
 library(units)
 library(RColorBrewer)
 library(stringr)
+library(ggspatial)
+library(units)
+library(RColorBrewer)
 
-#---- Obtendo bases de dados
-# Pacote geobr: dados sobre municipíos e UCs do estado em formato 'sf'
+#---- Obtendo bases de dados---------------------------------------------------------------
+## Pacote geobr: dados sobre municipíos e UCs do estado em formato 'sf'
 # Municípios
 municipios_RJ <- read_municipality() %>%
   filter(abbrev_state == "RJ") %>%
@@ -35,7 +38,6 @@ UC <- read_conservation_units() %>%
 
 UC$area_uc<-gsub(" [m^2]", "", UC$area_uc)
 UC$area_uc<-as.numeric(UC$area_uc)
-
 
 # Realizar a interseção espacial entre UCs e os municípios do Rio de Janeiro
 UC_RJ <- st_intersection(UC1, municipios_RJ)%>% 
@@ -59,11 +61,46 @@ UC_RJ2<- UC_RJ %>%
     geom = st_union(geom)                 # Combinar as geometrias
   )
 
-# Filtrar as linhas que contêm a palavra "PARQUE" na coluna 'nome_uc'
+# Filtrar as linhas que contêm a palavra "PARQUE ESTADUAL" 
 Parque_RJ <- UC_RJ2 %>%
   filter(str_detect(name_conservation_unit, "PARQUE ESTADUAL"))
 
+#mapa das UCs com o limite dos municipios
+ggplot() +
+  geom_sf(data = municipios_RJ, fill = "white", color = "black") + # Fundo dos municípios
+  geom_sf(data = Parque_RJ, fill = 'green', color = "black") + # Parques
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "aliceblue"),
+    strip.text = element_text(size = 12, face = "bold")
+  ) +
+  annotation_scale(location = "br") + # Escala no canto inferior direito
+  annotation_north_arrow(location = "tl", which_north = "true", style = north_arrow_fancy_orienteering)
+
+## MAPBiomas: série histórica parques estaduais RJ (1985-2023)
+UC_Mapbiomas<-read_excel("Mapbiomas.xlsx", sheet=2)
+
+# Filtrar as linhas que contêm a palavra "PARQUE ESTADUAL" 
+Parques_Mapbiomas <- UC_Mapbiomas %>%
+  filter(str_detect(territory_level3, "PARQUE ESTADUAL"))
+  
+Parques_Mapbiomas<-Parques_Mapbiomas[,-c(12:40)]
+colnames(Parques_Mapbiomas)[4]<-c("name_conservation_unit")
+
+# Comparar os nomes de UCs entre as duas bases usando uma métrica de distância de strings
+distancias <- stringdist::stringdistmatrix(Parque_RJ$name_conservation_unit, Parques_Mapbiomas$territory_level3, method = "jw")
+
+# Agora você pode identificar quais pares têm a menor distância e, assim, encontrar correspondências
+# Para fins de demonstração, vamos encontrar a correspondência mais próxima para cada nome em UC1
+matches <- apply(distancias, 1, function(x) which.min(x))
+
+# Primeiro, criar uma coluna de listas de correspondências
+# Ajustar o código para usar seq_along corretamente com rowwise e mutate
+
+Parques_Mapbiomas2<-Parques_Mapbiomas[c(180:190,622:629,1053:1060,1061:1064,612:621,1395:1405,29:35,243:254,191:193),]
+# !!!!Estão faltando os parques estaduais dos três picos e da pedra branca que não aparecem em nehuma das bases de dados
 
 
-# MAPBiomas: série histórica parques estaduais RJ (1985-2023)
-UC<-read_excel("Mapbiomas.xlsx", sheet=2)
+##### Estatísticas Descritivas ------------------------------------------------------------------------------
+
+
